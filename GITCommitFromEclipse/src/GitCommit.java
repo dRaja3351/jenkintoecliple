864ed64e8083035189121f36624f96d26be54342
draja3351 <draja2833@gmail.com>
2023-10-29 10:59:48 +0530

    adding code

--- GITCommitFromEclipse/src/GitCommit.java
+++ GITCommitFromEclipse/src/GitCommit.java
@@ -5,6 +5,7 @@ public class GitCommit {
 		System.out.println("Welcome to CI process");
 		System.out.println("Commiting the code to git from Eclipse");
 		System.out.println("Commiting source into jenkins");
+		System.out.println("Testing");
 
 	}
 
