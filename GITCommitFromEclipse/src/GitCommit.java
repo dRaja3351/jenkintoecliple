ca95008747aefd6ace1c87d9cff60613f2d8dbdf
draja3351 <draja2833@gmail.com>
2023-10-29 10:54:30 +0530

    Commiting the code to git

---
+++ GITCommitFromEclipse/src/GitCommit.java
@@ -0,0 +1,11 @@
+
+public class GitCommit {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to CI process");
+		System.out.println("Commiting the code to git from Eclipse");
+		System.out.println("Commiting source into jenkins");
+
+	}
+
+}
